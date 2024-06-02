2a933c74ef3f67238b79ab5037d89f8edbeccc71
Adyan Muhammad <adyanmuhammad932gmail.com>
2024-06-03 01:43:53 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NonAuthorizeView from "../views/NonAuthorizeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -121,7 +122,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/NonAuthorizeView.vue"),
+      component: NonAuthorizeView,
     },
   ],
 });
